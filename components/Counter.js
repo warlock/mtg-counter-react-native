e65be1f2b6011ee87f92ef3ba3737d35c0c3bcac
Josep Subils <js@js.gl>
2019-04-14 08:29:06 +0200

    [up] big button and rotated

--- components/Counter.js
+++ components/Counter.js
@@ -12,50 +12,59 @@ class Counter extends PureComponent {
     return (
       <ImageBackground
         source={this.props.img}
-        style={{
-          width: '100%',
-          height: '50%',
-          alignItems: 'center',
-          width: width / 2,
-          backgroundColor: 'red',
-          opacity: this.props.life < 1 || this.props.poison > 9 ? 0.4 : 1
-        }}
+        style={[styles.background, { opacity: this.props.life < 1 || this.props.poison > 9 ? 0.4 : 1 }]}
       >
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            width: width / 2,
-            height: '80%'
-          }}
-        >
-          {this.state.life ? (
-            <TouchableOpacity onPress={() => this.props.downlife()}>
-              <View>
-                <Text style={[styles.text, { color: 'lightgreen' }]}>{this.props.life}</Text>
-              </View>
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity onPress={() => this.props.poisonchange(true)}>
-              <View>
-                <Text style={[styles.text, { color: 'magenta' }]}>{this.props.poison}</Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        </View>
+        {this.state.life ? (
+          <TouchableOpacity
+            style={styles.opacity}
+            onPress={() => this.props.downlife()}
+            hitSlop={{ top: 10, bottom: 10, left: 0, right: width / 2 }}
+          >
+            <View>
+              <Text
+                style={[
+                  styles.text,
+                  {
+                    color: 'lightgreen',
+                    transform: this.props.up ? [{ rotate: '180deg' }] : []
+                  }
+                ]}
+              >
+                {this.props.life}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={() => this.props.poisonchange(true)}>
+            <View>
+              <Text style={[styles.text, { color: 'magenta' }]}>{this.props.poison}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
       </ImageBackground>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  text: { fontSize: 80 },
+  text: {
+    fontSize: 80
+  },
   textsmall: { fontSize: 25 },
   lines: {
     borderWidth: 3,
     borderColor: 'red'
+  },
+  opacity: {
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  background: {
+    width: '100%',
+    height: '50%',
+    alignItems: 'center',
+    backgroundColor: 'red'
   }
 })
 
