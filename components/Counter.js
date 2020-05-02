1c51018b00d7e539c087478f0e5a194620e4ee9a
Josep Subils <js@js.gl>
2020-05-02 09:48:19 +0200

    Fix poison up and add symbols

--- components/Counter.js
+++ components/Counter.js
@@ -7,9 +7,18 @@ import {
   TouchableOpacity,
   ImageBackground
 } from 'react-native'
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, AntDesign } from '@expo/vector-icons'
 
-export default ({ img, life, poison, downlife, downpoison, uplife, up }) => {
+export default ({
+  img,
+  life,
+  poison,
+  downlife,
+  downpoison,
+  uplife,
+  uppoison,
+  up
+}) => {
   const [switcher, setSwitcher] = useState(true)
   const { width } = Dimensions.get('screen')
   return (
@@ -44,7 +53,19 @@ export default ({ img, life, poison, downlife, downpoison, uplife, up }) => {
               downpoison()
             }
           }}
-        />
+        >
+          <AntDesign
+            style={{
+              position: 'absolute',
+              zIndex: 3,
+              alignSelf: 'center',
+              transform: up ? [{ rotate: '180deg' }] : []
+            }}
+            name="minuscircleo"
+            size={35}
+            color="white"
+          />
+        </TouchableOpacity>
         <TouchableOpacity
           style={styles.opacityr}
           onPress={() => {
@@ -54,12 +75,24 @@ export default ({ img, life, poison, downlife, downpoison, uplife, up }) => {
               uppoison()
             }
           }}
-        />
+        >
+          <AntDesign
+            style={{
+              position: 'absolute',
+              zIndex: 3,
+              alignSelf: 'center',
+              transform: up ? [{ rotate: '180deg' }] : []
+            }}
+            name="pluscircleo"
+            size={35}
+            color="white"
+          />
+        </TouchableOpacity>
         <Text
           style={{
             position: 'absolute',
             zIndex: 0,
-            fontSize: width / 2,
+            fontSize: width / 3,
             color: switcher ? 'lightgreen' : 'magenta',
             transform: up ? [{ rotate: '180deg' }] : [],
             textShadowColor: 'rgba(0, 0, 0, 0.75)',
@@ -93,7 +126,11 @@ const styles = StyleSheet.create({
     height: '100%',
     zIndex: 1,
     //backgroundColor: 'red',
-    left: 0
+    left: 0,
+    justifyContent: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 10
   },
   opacityr: {
     position: 'absolute',
@@ -101,7 +138,11 @@ const styles = StyleSheet.create({
     height: '100%',
     zIndex: 1,
     //backgroundColor: 'blue',
-    right: 0
+    right: 0,
+    justifyContent: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 10
   },
   background: {
     width: '100%',
