9c1b76315bb5ce41890ccbf6fd7355a997a3bc5f
Josep Subils <js@js.gl>
2020-05-24 15:05:04 +0200

    Load more icons

--- hooks/useCachedResources.js
+++ hooks/useCachedResources.js
@@ -1,4 +1,4 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
 import * as Font from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 import * as React from 'react'
@@ -6,19 +6,15 @@ import * as React from 'react'
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false)
 
-  // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
         SplashScreen.preventAutoHideAsync()
-
-        // Load fonts
         await Font.loadAsync({
-          ...MaterialCommunityIcons.font
-          //          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf')
+          ...MaterialCommunityIcons.font,
+          ...AntDesign.font
         })
       } catch (e) {
-        // We might want to provide this error information to an error reporting service
         console.warn(e)
       } finally {
         setLoadingComplete(true)
